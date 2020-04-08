439f420997457f126a87115d7515731840c01525
Scout2012 <jcbpowell12@gmail.com>
2020-04-07 19:44:35 -0700

    Added input file error handling

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        Syntax syntax = new Syntax(args[0]);
+    	try{
+    		Syntax syntax = new Syntax(args[0]);    		
+    	} catch(Exception e){
+    		System.out.println("Please specify an input file in the command line");
+    	}
     }
 }
